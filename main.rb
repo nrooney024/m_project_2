6edb739e03ed455c7dcd053adf2190531aa736dc
nrooney024 <nrooney024@gmail.com>
2023-08-24 13:09:59 +0100

    Making main.rb drier, by adding loops instead of writing things out multiple times. Also, passing true as an argument to the sort methods, to let them know to display the sort in the terminal.

--- main.rb
+++ main.rb
@@ -5,35 +5,31 @@ require_relative 'db_manager'
 # To help organize the results in the terminal
 line_break = "\n\n"
 
+# Configuring how each file should be processed by FileManager
+file_configs = [
+	['./input_files/file1.txt', ' ', {last_name: 0, first_name: 1, gender: 3, dob: 4, color: 5}],
+	['./input_files/file2.txt', '|', {last_name: 0, first_name: 1, gender: 3, dob: 5, color: 4}],
+	['./input_files/file3.txt', ',', {last_name: 0, first_name: 1, gender: 2, dob: 4, color: 3}]
+]
 
-# Creating FileManager instances for each txt file of personal data
-file1_manager = FileManager.new('./input_files/file1.txt', ' ', {last_name: 0, first_name: 1, gender: 3, dob: 4, color: 5})
-file2_manager = FileManager.new('./input_files/file2.txt', '|', {last_name: 0, first_name: 1, gender: 3, dob: 5, color: 4})
-file3_manager = FileManager.new('./input_files/file3.txt', ',', {last_name: 0, first_name: 1, gender: 2, dob: 4, color: 3})
+# Construct person arrays from the file configurations by making FileManager instances 
+# and calling the file_to_person_array method on them
+person_arrays = []
+file_configs.each do |file_config|
+  person_arrays << FileManager.new(*file_config).file_to_person_array	
+end
 
+# Create DB Manager instance
+db_manager = DBManager.new(person_arrays)
 
-# Creating arrays of Person instances from the personal data in the txt files with file_to_person_array method
-file1_persons = file1_manager.file_to_person_array
-file2_persons = file2_manager.file_to_person_array
-file3_persons = file3_manager.file_to_person_array
+# Gathering which sorts we'd like to apply
+db_sorts = [
+	:sort_by_gender_and_last_name,
+	:sort_by_dob_and_last_name,
+	:sort_by_last_name_desc
+]
 
-# Combine all Person arrays into one in DBManager class
-db_manager = DBManager.new([file1_persons, file2_persons, file3_persons])
-
-
-# Sort and display the data using DBManager methods
-puts "Output 1: sorted by gender (females before males) then by last name ascending:"
-
-puts db_manager.sort_by_gender_and_last_name.map(&:to_standardized_string)
-
-puts line_break
-
-puts "Output 2: sorted by birth date, ascending then by last name ascending"
-
-puts db_manager.sort_by_dob_and_last_name.map(&:to_standardized_string)
-
-puts line_break
-
-puts "Output 3: sorted by last name, descending"
-
-puts db_manager.sort_by_last_name_desc.map(&:to_standardized_string)
+# Applying sorts and passing true, so we can see the sorts in the terminal
+db_sorts.each do |sort_method|
+  db_manager.send(sort_method, true) 
+end
